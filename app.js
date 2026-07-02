9d65223f3a3a45e1bcd404fcc8708465c44011ea
wubdigital <wubdigitalinfo@gmail.com>
2026-07-02 16:42:41 +0000

    sharped-server

--- app.js
+++ app.js
@@ -5,15 +5,52 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 const usersRoutes = require("./routes/users");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const alloweOrigins = [
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+  process.env.RENDER_EXTERNAL_URL,
+].filter(Boolean);
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || alloweOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("not allowe by CORS policies"));
+      }
+    },
+  }),
+);
 
 connectDB();
+
+if (!process.env.JWT_SECRET) {
+  console.error("JWT_SECRET,is not defined in enviroment varaiables");
+  process.exit(1);
+}
+
+app.get("/", (req, res) => {
+  res.send("wellcome to our users management app!!");
+});
+
+app.get("/health", (req, res) => {
+  const dbStates = ["connected", "disconnected", "connecting", "disconnecting"];
+  const dbconnected = mongoose.connection.readyState === 1;
+
+  res.status(dbconnected ? 200 : 503).json({
+    status: dbconnected ? "ok" : "error",
+    db: dbStates[mongoose.connection.readyState],
+    runtime: `${Math.floor(process.uptime)}s`,
+    environment: process.env.NODE_ENV || "development",
+  });
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
